9dbc7a0e3b2390ec1a2e480e62eff6e4c18212f1
ngireric123 <ngireric123@gmail.com>
2022-05-25 23:14:36 +0200

    install Gemfile.lock file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     require 'net/http'
     def index
-        url = "https://api.openweathermap.org/data/2.5/weather?lat=33.7490&lon=-84.38806&appid=4160b8edff7b312aef70efc4f687e18d"
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=-1.935114&lon=30.082111&appid=4160b8edff7b312aef70efc4f687e18d"
 
         uri = URI(url)
         res = Net::HTTP.get_response(uri)
