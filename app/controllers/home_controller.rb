db963b8aa4ae8150d8188e455e41039e81bbd4ca
ngireric123 <ngireric123@gmail.com>
2022-05-25 18:41:35 +0200

    add feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
+    require 'net/http'
     def index
-        url = "https://api.openweathermap.org/data/2.5/weather"
+        url = "https://api.openweathermap.org/data/2.5/weather?lat=33.7490&lon=-84.38806&appid=4160b8edff7b312aef70efc4f687e18d"
+
+        uri = URI(url)
+        res = Net::HTTP.get_response(uri)
+        @data = JSON.parse(res.body)
     end
 end
